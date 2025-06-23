c9e896339f40deb426c4ae7a5dffb4eb6c64dd6f
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-06-23 16:32:41 -0300

    login screen being implemented

--- src/app/screens/welcome_screen.tsx
+++ src/app/screens/welcome_screen.tsx
@@ -1,10 +1,14 @@
+import { Button } from '@/src/components/ui/button';
 import { SafeAreaView, StatusBar, Text } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Index() {
   return (
-    <SafeAreaView>
+    <SafeAreaView className={'w-screen h-screen bg-secondary-100'} >
       <StatusBar />
+      <MaterialCommunityIcons name={'soccer'} size={200} color={'black'} />
       <Text>Welcome to MyoroPlayer</Text>
+      <Button><Text>FUck off gallager</Text></Button>
     </SafeAreaView>
-  );
+  )
 }
---
+++ src/theme/colors.ts
@@ -0,0 +1,6 @@
+const colors = {
+  primary: '#181818',
+  onPrimary: '#EDE6D6',
+}
+
+export default colors
