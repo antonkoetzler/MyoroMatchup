f12b9d05feff87ad737ffdb8e74446ed415aeca7
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-11-03 21:14:45 -0300

    okay

--- myoro_matchup_api/src/main/java/com/myoro/myoro_matchup_api/config/SecurityConfig.java
+++ myoro_matchup_api/src/main/java/com/myoro/myoro_matchup_api/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
             .authenticationEntryPoint(customAuthenticationEntryPoint))
         .authorizeHttpRequests(authz -> authz
             .requestMatchers("/api/auth/**").permitAll()
-            .requestMatchers("/api/user/**").permitAll()
+            .requestMatchers("/api/users/**").permitAll()
             .requestMatchers("/").permitAll()
             .requestMatchers("/api/test/**").permitAll()
             .requestMatchers("/swagger-ui/**").permitAll()
